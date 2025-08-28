7f4e8dfe2db32a6efd8c55e75a21e1cd07a01bed
nathanbailey1 <nbaile02@student.ubc.ca>
2025-08-28 10:59:26 -0700

    Update test_standard_calc.py

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,6 +6,11 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
+    assert bound_to_180(180) == 180
+    assert bound_to_180(200) == -160
+    assert bound_to_180(-180) == -180
+    assert bound_to_180(-200) == 160
+    assert bound_to_180(369) == 9
 
 
 """ Tests for is_angle_between() """
@@ -13,3 +18,7 @@ def test_bound_basic1():
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+    assert is_angle_between(0, -1, 2)
+    assert is_angle_between (0, 3, 2)
+    assert is_angle_between (0, 2, 2)
+    assert is_angle_between (0, 0, 2)
